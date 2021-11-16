03a1158883a9508e478223a70f93a11a3ee70d09
kiaplayer <kiaplayer@gmial.com>
2021-11-16 23:12:19 +0300

    Added golang backend

--- server.go
+++ server.go
@@ -5,13 +5,38 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+
+	"github.com/kiaplayer/vue-go-backend/endpoints"
 )
 
 func main() {
 	r := mux.NewRouter()
+	r = endpoints.AddRouterEndpoints(r)
 	fs := http.FileServer(http.Dir("./dist"))
 	r.PathPrefix("/").Handler(fs)
-	http.Handle("/", r)
+
+	http.Handle("/", &corsRouterDecorator{r})
 	fmt.Println("Listening")
-	log.Panic(http.ListenAndServe(":3000", nil))
+	log.Panic(
+		http.ListenAndServe(":3000", nil),
+	)
+}
+
+type corsRouterDecorator struct {
+	R *mux.Router
+}
+
+func (c *corsRouterDecorator) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if origin := req.Header.Get("Origin"); origin != "" {
+		rw.Header().Set("Access-Control-Allow-Origin", origin)
+		rw.Header().Set("Access-Control-Allow-Methods",
+			"POST, GET, PUT, DELETE, PATCH")
+		rw.Header().Add("Access-Control-Allow-Headers",
+			"Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+	}
+	// Stop here if its Preflighted OPTIONS request
+	if req.Method == "OPTIONS" {
+		return
+	}
+	c.R.ServeHTTP(rw, req)
 }
